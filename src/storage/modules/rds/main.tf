629d271371f343a90d1d72ffe04991eabab04631
Javier Vazquez Fernandez <javier@MacBook-Pro-de-Javier-2.local>
2023-01-13 13:18:55 +0100

    Add final snapshot to rds

--- src/storage/modules/rds/main.tf
+++ src/storage/modules/rds/main.tf
@@ -4,19 +4,19 @@ resource "aws_db_subnet_group" "this" {
 }
 
 resource "aws_db_instance" "this" {
-  identifier             = lower(var.db_name)
-  db_name                = var.db_name
-  instance_class         = var.db_instance_class
-  allocated_storage      = var.storage
-  engine                 = var.engine.name
-  engine_version         = var.engine.version
-  publicly_accessible    = false
-  vpc_security_group_ids = var.security_group_ids
-  username               = var.username
-  password               = var.password
-  port                   = var.port
-  db_subnet_group_name   = aws_db_subnet_group.this.name
-  skip_final_snapshot    = true
+  identifier                = lower(var.db_name)
+  db_name                   = var.db_name
+  instance_class            = var.db_instance_class
+  allocated_storage         = var.storage
+  engine                    = var.engine.name
+  engine_version            = var.engine.version
+  publicly_accessible       = false
+  vpc_security_group_ids    = var.security_group_ids
+  username                  = var.username
+  password                  = var.password
+  port                      = var.port
+  db_subnet_group_name      = aws_db_subnet_group.this.name
+  final_snapshot_identifier = "${lower(var.db_name)}-snapshot"
 }
 
 output "endpoint" {
